ac85d5ae00f3840a237cb9d0552a4795e05823de
Agustin Zubiaga <hi@aguz.me>
2022-07-05 14:25:21 -0400

    Accept connections

--- make_const.c
+++ make_const.c
@@ -1,4 +1,7 @@
+#include <netinet/in.h>
 #include <stdio.h>
+#include <sys/_types/_iovec_t.h>
+#include <sys/socket.h>
 #include <sys/syscall.h>
 
 int main() {
@@ -6,10 +9,33 @@ int main() {
 
   fprintf(fp, "// Do not manually edit this file\n\n");
 
-  #define add_const(name) fprintf(fp, ".equ %s, %d\n", #name, name);
+#define add_const(name) fprintf(fp, ".equ %s, %d\n", #name, name);
 
   add_const(SYS_exit);
   add_const(SYS_write);
+  add_const(SYS_socket);
+  add_const(SYS_setsockopt);
+  add_const(SYS_bind);
+  add_const(SYS_listen);
+  add_const(SYS_accept);
+  add_const(SYS_close);
+  add_const(AF_INET);
+  add_const(PF_INET);
+  add_const(SOCK_STREAM);
+  add_const(INADDR_ANY);
+  add_const(SOL_SOCKET);
+  add_const(SO_REUSEADDR);
+  add_const(SO_REUSEPORT);
+
+  fprintf(fp, ".equ sizeof_int, %lu\n", sizeof(int));
+
+  struct sockaddr_in _sockaddr_in;
+  fprintf(fp, ".equ sizeof_sockaddr_in, %lu\n", sizeof(_sockaddr_in));
+
+  struct sockaddr_storage _sockaddr_storage;
+  fprintf(fp, ".equ sizeof_sockaddr_storage, %lu\n", sizeof(_sockaddr_storage));
+
+  fprintf(fp, ".equ PORT, %d\n", htons(4520));
 
   fclose(fp);
 
--- server.s
+++ server.s
@@ -1,5 +1,13 @@
 .include "out/const.s"
 
+.data
+
+my_addr: 
+    .ds sizeof_sockaddr_in
+
+their_addr: 
+    .ds sizeof_sockaddr_storage
+
 .text
 
 .global _start
@@ -10,6 +18,86 @@ _start:
     mov     x1, 4
     bl      print
 
+    // Get listener sock fd
+    mov     x0, PF_INET
+    mov     x1, SOCK_STREAM
+    mov     x2, 0
+    mov     x16, SYS_socket
+    svc     0
+
+    // Save returned sockfd
+    mov     x10, x0
+
+    // Allow reusing address
+    mov     x0, x10
+    mov     x1, SOL_SOCKET
+    mov     x2, SO_REUSEADDR
+    adr     x3, true
+    mov     x4, sizeof_int
+    mov     x16, SYS_setsockopt
+    svc     0
+    
+    // Allow reusing port
+    mov     x0, x10
+    mov     x1, SOL_SOCKET
+    mov     x2, SO_REUSEPORT
+    adr     x3, true
+    mov     x4, sizeof_int
+    mov     x16, SYS_setsockopt
+    svc     0
+    
+    // Construct sockaddr_in struct for bind
+    adrp    x8, my_addr@page
+    add     x8, x8, my_addr@pageoff
+    mov     w9, 0           // my_addr.sin_len = 0
+    strb    w9, [x8]  
+    mov     w9, AF_INET     // my_addr.sin_family = AF_INET
+    strb    w9, [x8, 1]
+    mov     w9, PORT        // my_addr.sin_port = PORT
+    strh    w9, [x8, 2]
+    mov     w9, INADDR_ANY  // my_addr.sin_addr.s_addr = INADDR_ANY
+    strb    w9, [x8, 4]
+    mov     w9, 0           // my_addr.sin_zero = 0
+    strb    w9, [x8, 8]      
+
+    // Bind port
+    mov     x0, x10
+    mov     x1, x8
+    mov     x2, sizeof_sockaddr_in 
+    mov     x16, SYS_bind
+    svc     0
+    
+    // Listen
+    mov     x0, x10
+    mov     x1, 10
+    mov     x16, SYS_listen
+    svc     0
+
+    // Accept
+    adrp    x11, their_addr@page
+    add     x11, x11, their_addr@pageoff
+    mov     x0, x10
+    mov     x1, x11
+    adr     x2, sizeof_sockaddr_storage_p
+    mov     x16, SYS_accept
+    svc     0
+
+    // Store sockfd for this specific connection
+    mov     x12, x0
+
+    // TODO: Handle request
+
+    // Close connection fd
+    mov     x0, x12
+    mov     x16, SYS_close
+    svc     0
+    
+    // Close listening fd
+    mov     x0, x10
+    mov     x16, SYS_close
+    svc     0
+
+    // Exit 
     mov     x0, 0
     mov     x16, SYS_exit
     svc     0
@@ -17,10 +105,16 @@ _start:
 print:
     mov     x2, x1
     mov     x1, x0
-    mov     x0, #1
+    mov     x0, 1
     mov     x16, SYS_write
     svc     0
     ret
 
 hi:
     .ascii  "Hi!\n"
+
+true:
+    .word   1
+
+sizeof_sockaddr_storage_p:
+    .word   sizeof_sockaddr_storage
